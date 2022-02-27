95b0bdabb710445f71d8a2891731dd8df86f779c
Rita <94860684+ritullia@users.noreply.github.com>
2022-02-27 12:16:59 +0200

    new code

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const randomImageUrl = 'https://dog.ceo/api/breeds/image/random';
 const rezultatas = document.querySelector('#rezultatas');
 const breedsListUrl = 'https://dog.ceo/api/breeds/list/all'
 
+
 const getImage = (url, random = true) => {
     fetch(url)
         .then(response => response.json())
@@ -26,20 +27,32 @@ const getBreed = (url) => {
             select.setAttribute('name', 'breedsList')
             document.querySelector('.select-form').appendChild(select)
 
-            let breedsList = document.querySelector('select[name=breedsList]')
+            let breedsList = document.querySelector('select[name="breedsList"]')
             let emptyOption = document.createElement('option')
             emptyOption.textContent = 'Pasirinkite veisle'
+
             select.appendChild(emptyOption)
 
-
-            Object.entries(data).forEach((value) => {
-                console.log(value.keys(0))
+            Object.keys(data).forEach((value) => {
+                // console.log(value, data[value])
                 let breed = document.createElement('option')
-                breed.textContent = value
-                breedsList.appendChild()
-            });
-        });
+                breed.textContent = value;
+                breedsList.appendChild(breed)
 
+                emptyOption.textContent = value;
+
+                let subbreed = (data[value])
+                subbreed.forEach((obj) => {
+                    console.log(value + ' ' + obj)
+                    let subBreed = document.createElement('option')
+                    subBreed.textContent = `${value} ${obj}`;
+                    subBreed.value = `${value}/${obj}`;
+
+                    breedsList.appendChild(subBreed);
+                    console.log(`https://dog.ceo/api/breed/${value}/${obj}/images/random`)
+                })
+            })
+        })
 }
 
 
@@ -61,14 +74,28 @@ document.querySelector('.findBreed').addEventListener('click', () => {
 
 
 document.querySelector('.select').addEventListener('click', () => {
-    let breed = document.querySelector('select[name="breedList"]').value;
+    let breed = document.querySelector('select[name="breedsList"]').value;
     if (breed === "") {
         rezultatas.textContent = 'Neįvesta jokia veislė'
         return false
-    }
-
+    };
+    let parsedBreed = breed.toLowerCase()
     let breedUrl = `https://dog.ceo/api/breed/${parsedBreed}/images/random`
     selectBreed(breedUrl, false)
+
+})
+
+// pakeisti veisle
+document.querySelector('.select').addEventListener('change', () => {
+    let breed = document.querySelector('select[name="breedsList"]').value;
+    if (breed === "") {
+        rezultatas.textContent = 'Neįvesta jokia veislė'
+        return false
+    };
+    let parsedBreed = breed.toLowerCase()
+    let breedUrl = `https://dog.ceo/api/breed/${parsedBreed}/images/random`
+    selectBreed(breedUrl, false)
+
 })
 
 
